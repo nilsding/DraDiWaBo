89565ab222354a3ffb14e1d50a32799034b89b4d
Georg Gadinger <nilsding@nilsding.org>
2018-09-16 18:05:16 +0200

    Use `_spec` postfix for specs

--- spec/repository/watch_targets_spec.cr
+++ spec/repository/watch_targets_spec.cr
@@ -6,64 +6,64 @@ require "../../src/repository/watch_targets"
 module Repository
   class WatchTargetsTest < Minitest::Test
     def test_add
-      redis.hset "ddw:wt:nova", "Extralarge", "[420]"
-      Repository::WatchTargets.add(1337, "nova", BadDragon::Size::Extralarge)
+      redis.hset "ddw:wt:nova_spec", "Extralarge", "[420]"
+      Repository::WatchTargets.add(1337, "nova_spec", BadDragon::Size::Extralarge)
 
-      users = JSON.parse(redis.hget("ddw:wt:nova", "Extralarge") || "[]").as_a.map(&.as_i)
+      users = JSON.parse(redis.hget("ddw:wt:nova_spec", "Extralarge") || "[]").as_a.map(&.as_i)
       assert_equal [420, 1337], users
     end
 
     def test_add_same_user
-      redis.hset "ddw:wt:nova", "Extralarge", "[420, 1337]"
-      Repository::WatchTargets.add(1337, "nova", BadDragon::Size::Extralarge)
+      redis.hset "ddw:wt:nova_spec", "Extralarge", "[420, 1337]"
+      Repository::WatchTargets.add(1337, "nova_spec", BadDragon::Size::Extralarge)
 
-      users = JSON.parse(redis.hget("ddw:wt:nova", "Extralarge") || "[]").as_a.map(&.as_i)
+      users = JSON.parse(redis.hget("ddw:wt:nova_spec", "Extralarge") || "[]").as_a.map(&.as_i)
       assert_equal [420, 1337], users
     end
 
     def test_add_nonexistent_key
-      redis.del "ddw:wt:nova"
-      Repository::WatchTargets.add(1337, "nova", BadDragon::Size::Extralarge)
+      redis.del "ddw:wt:nova_spec"
+      Repository::WatchTargets.add(1337, "nova_spec", BadDragon::Size::Extralarge)
 
-      users = JSON.parse(redis.hget("ddw:wt:nova", "Extralarge") || "[]").as_a.map(&.as_i)
+      users = JSON.parse(redis.hget("ddw:wt:nova_spec", "Extralarge") || "[]").as_a.map(&.as_i)
       assert_equal [1337], users
     end
 
     def test_remove
-      redis.hset "ddw:wt:nova", "Extralarge", "[1337, 69, 420]"
-      Repository::WatchTargets.remove(69, "nova", BadDragon::Size::Extralarge)
+      redis.hset "ddw:wt:nova_spec", "Extralarge", "[1337, 69, 420]"
+      Repository::WatchTargets.remove(69, "nova_spec", BadDragon::Size::Extralarge)
 
-      users = JSON.parse(redis.hget("ddw:wt:nova", "Extralarge") || "[]").as_a.map(&.as_i)
+      users = JSON.parse(redis.hget("ddw:wt:nova_spec", "Extralarge") || "[]").as_a.map(&.as_i)
       assert_equal [1337, 420], users
     end
 
     def test_remove_nonexistent_user
-      redis.hset "ddw:wt:nova", "Extralarge", "[1337, 69, 420]"
-      Repository::WatchTargets.remove(666, "nova", BadDragon::Size::Extralarge)
+      redis.hset "ddw:wt:nova_spec", "Extralarge", "[1337, 69, 420]"
+      Repository::WatchTargets.remove(666, "nova_spec", BadDragon::Size::Extralarge)
 
-      users = JSON.parse(redis.hget("ddw:wt:nova", "Extralarge") || "[]").as_a.map(&.as_i)
+      users = JSON.parse(redis.hget("ddw:wt:nova_spec", "Extralarge") || "[]").as_a.map(&.as_i)
       assert_equal [1337, 69, 420], users
     end
 
     def test_remove_nonexistent_key
-      redis.del "ddw:wt:nova"
-      Repository::WatchTargets.remove(1337, "nova", BadDragon::Size::Extralarge)
+      redis.del "ddw:wt:nova_spec"
+      Repository::WatchTargets.remove(1337, "nova_spec", BadDragon::Size::Extralarge)
 
-      redis_response = redis.hget("ddw:wt:nova", "Extralarge")
+      redis_response = redis.hget("ddw:wt:nova_spec", "Extralarge")
       assert_equal "[]", redis_response
     end
 
     def test_watchers
-      redis.hset "ddw:wt:nova", "Extralarge", "[1337, 69, 420]"
+      redis.hset "ddw:wt:nova_spec", "Extralarge", "[1337, 69, 420]"
 
-      users = Repository::WatchTargets.watchers("nova", BadDragon::Size::Extralarge)
+      users = Repository::WatchTargets.watchers("nova_spec", BadDragon::Size::Extralarge)
       assert_equal [1337, 69, 420], users
     end
 
     def test_watchers_nonexistent_key
-      redis.del "ddw:wt:nova"
+      redis.del "ddw:wt:nova_spec"
 
-      users = Repository::WatchTargets.watchers("nova", BadDragon::Size::Extralarge)
+      users = Repository::WatchTargets.watchers("nova_spec", BadDragon::Size::Extralarge)
       assert_equal [] of Int32, users
     end
 
